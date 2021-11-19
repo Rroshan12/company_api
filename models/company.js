afafb2a997e24a04f0f54dc3bdf61b6ee210cf1d
Rroshan12 <grappleroshan7@gmail.com>
2021-11-19 18:47:28 +0545

    schema are coded

---
+++ models/company.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const companySchema = mongoose.Schema({
+    category_id:[
+        {type: mongoose.Schema.Types.ObjectId, ref: 'Category'}
+      ],
+  title: String,
+  image: String,
+  descrpition: String,
+  status: Boolean,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+},
+updatedAt: {
+    type: Date,
+    default: new Date(),
+},
+
+
+
+
+
+})
+
+var Company = mongoose.model('Company', companySchema);
+
+export default companySchema;
---
+++ models/company_category.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const categorySchema = mongoose.Schema({
+
+  title: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+},
+updatedAt: {
+    type: Date,
+    default: new Date(),
+},
+
+
+
+
+
+})
+
+var Category  = mongoose.model('Category', categorySchema);
+
+export default categorySchema ;
